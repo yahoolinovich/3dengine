670e75ebebf6af31276f23ae79be9d70f0385000
yahoolinovich <106834198+yahoolinovich@users.noreply.github.com>
2022-09-08 18:15:28 -0400

    Update obj.py

--- obj.py
+++ obj.py
@@ -3,16 +3,38 @@ import numpy as np
 import pygame
 
 class Object:
-    def __init__(self, eng):
+    def __init__(self, eng, file_name):
         self.eng = eng
-        self.vertices = np.array([
-            [-1,-1,1,1],[-1,-1,-1,1],[-1,1,1,1],[-1,1,-1,1],
-            [1,1,1,1],[1,1,-1,1],[1,-1,1,1],[1,-1,-1,1]
-        ])
-        self.faces = np.array([
-            [0,1,3,2],[3,2,4,5],[4,5,7,6],[7,6,0,1],[0,2,4,6],[7,1,3,5]
-        ])
+        self.file_name = file_name
+        self.vertices, self.faces = self.open_obj(self.file_name)
+        print(f'Vertices: \n')
+        print(f'\nFaces: \n')
+        # self.vertices = np.array([
+        #     [-1,-1,1,1],[-1,-1,-1,1],[-1,1,1,1],[-1,1,-1,1],
+        #     [1,1,1,1],[1,1,-1,1],[1,-1,1,1],[1,-1,-1,1]
+        # ])
+        # self.faces = np.array([
+        #     [0,1,3,2],[3,2,4,5],[4,5,7,6],[7,6,0,1],[0,2,4,6],[7,1,3,5]
+        # ])
 
+    def open_obj(self, filename):
+        vertices = []
+        faces = []
+        with open(filename) as file:
+            for line in file:
+                if line.startswith('v '):
+                    lines = line[2:].split() + [1]
+                    v = [float(i) for i in lines]
+                    vertices.append(v)
+                elif line.startswith('f '):
+                    lines_2 = line[2:].split()
+                    fc = [i.split('/') for i in lines_2]
+                    vf = []
+                    for sub_list in fc:
+                        vf.append(int(sub_list[0]))
+                        faces.append(vf)
+        return np.array(vertices), np.array(faces)
+    
     def rotation_x(self, angle):
         x = np.array([
             [1, 0, 0,0],
@@ -64,15 +86,15 @@ class Object:
     def draw(self):
         # self.vertices = np.dot(self.vertices,self.rotation_x(math.radians(1)))
         vertices = self.camera_relation()
-        print(f'1: {vertices[0]} \n')
+        # print(f'1: {vertices[0]} \n')
         vertices = np.dot(vertices, self.eng.cam.project_matrix())
-        print(f'2: {vertices[0]} \n')
+        # print(f'2: {vertices[0]} \n')
         vertices /= vertices[:, -1].reshape(-1, 1)
-        print(f'3: {vertices[0]} \n')
+        # print(f'3: {vertices[0]} \n')
         vertices = np.dot(vertices, self.eng.cam.screen_projection())
-        print(f'4: {vertices[0]} \n')
+        # print(f'4: {vertices[0]} \n')
         for i in vertices[:,:2]:
             pygame.draw.circle(self.eng.screen, pygame.Color('red'), i, 3)
         vix = vertices[:,:2]
         for face in self.faces:
-            pygame.draw.polygon(self.eng.screen, pygame.Color('white'), vix[face], 1)
+            pygame.draw.polygon(self.eng.screen, pygame.Color('white'), vix[face-1], 1)
