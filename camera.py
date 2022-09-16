310ab2a4ddb58e536ee971c8d54fcdf3785bb3c0
yahoolinovich <106834198+yahoolinovich@users.noreply.github.com>
2022-09-16 16:47:33 -0400

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,12 +9,13 @@ class Camera:
         self.pos = np.array([*pos, 0.0])
         self.H_FOV = math.pi / 3
         self.V_FOV = self.H_FOV * (eng.height / eng.width)
-        self.clip_near = 1
-        self.clip_far = 1000.0
+        self.clip_near = 100
+        self.clip_far = 10000.0
 
     def move(self):
         key = pygame.key.get_pressed()
-        speed = 1
+        mouse_pos_x, mouse_pos_y = pygame.mouse.get_rel()
+        speed = 10
         if key[pygame.K_s]:
             self.pos -= [0,0,speed,0]
         if key[pygame.K_w]:
@@ -23,6 +24,16 @@ class Camera:
             self.pos -= [speed,0,0,0]
         if key[pygame.K_d]:
             self.pos += [speed, 0, 0, 0]
+        if mouse_pos_y < 0:
+            self.eng.obj.vertices = self.eng.obj.vertices @ self.pitch(-0.015)
+        if mouse_pos_y > 0:
+            self.eng.obj.vertices = self.eng.obj.vertices @ self.pitch(0.015)
+        if mouse_pos_x > 0:
+            self.eng.obj.vertices = self.eng.obj.vertices @ self.eng.obj.rotation_y(-0.015)
+        if mouse_pos_x < 0:
+            self.eng.obj.vertices = self.eng.obj.vertices @ self.eng.obj.rotation_y(0.015)
+
+
 
     def normalize_x(self):
         x = fov(self.H_FOV)
@@ -42,6 +53,15 @@ class Camera:
             [0,self.normalize_y(),0,0],
             [0,0,self.normalize_z(),1],
             [0,0,(-self.clip_far * self.clip_near) / (self.clip_far - self.clip_near),0]#(self.clip_near * self.clip_far * 2) / (self.clip_near - self.clip_far),0]
+        ])
+
+    def pitch(self,a):
+        x,y,z,w = self.pos
+        return np.array([
+            [1,0,0,0],
+            [0,math.cos(a),-math.sin(a),0],
+            [0,math.sin(a),math.cos(a),0],
+            [0,0,0,1]
         ])
 
     def screen_projection(self):
