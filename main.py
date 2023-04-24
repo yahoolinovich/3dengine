4cee500202820037ff7899ddd590b55bc2d8a1db
Michael Nahmias <106834198+yahoolinovich@users.noreply.github.com>
2023-04-24 13:08:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import pygame
 from camera import *
 from obj import *
+from numba import njit
 
 
 class Engine:
@@ -11,22 +12,34 @@ class Engine:
         self.screen = pygame.display.set_mode(self.RES)
         self.clock = pygame.time.Clock()
         self.FPS = 60
-        self.cam = Camera(self,[0,0,0])
-        self.obj = Object(self, 'Glass desk.obj')
+        self.frame = np.ones((self.width, self.height, 3)).astype('uint8')
+        self.z_buffer = np.ones((self.width, self.height))
+        self.cam = Camera(self,[10,5.5,8])
+        self.objects = [Object(self, 'Glass desk.obj', '')]
+        
         pygame.event.set_grab(True)
         pygame.mouse.set_visible(False)
 
+
+
     def play(self):
-        self.obj.vertices = self.obj.vertices @ self.obj.translation([0,0,400])
+        light = np.asarray([0.5, 1, 1])
+
         while 1:
-        # for i in range(1):
+            light /= np.linalg.norm(light)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    exit()
+            self.z_buffer = 100000000
             self.screen.fill((0,0,0))
             self.cam.move()
-            self.obj.draw()
+            for i in self.objects:
+                i.draw()
+            # self.obj.draw()
             pygame.mouse.set_pos(400, 300)
             pygame.display.flip()
             self.clock.tick(self.FPS)
