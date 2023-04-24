3f169be67a0e7893287adeaa9a9d3183bda38c7b
Michael Nahmias <106834198+yahoolinovich@users.noreply.github.com>
2023-04-24 13:08:34 -0400

    Update obj.py

--- obj.py
+++ obj.py
@@ -4,38 +4,59 @@ import pygame
 
 
 class Object:
-    def __init__(self, eng, file_name):
+    def __init__(self, eng, file_name, texture):
         self.eng = eng
         self.file_name = file_name
-        self.vertices, self.faces = self.open_obj(self.file_name)
-        # print(f'Vertices: \n{self.vertices}')
-        # print(f'\nFaces: \n{self.faces}')
-        # self.vertices = np.array([
-        #     [-1,-1,1,1],[-1,-1,-1,1],[-1,1,1,1],[-1,1,-1,1],
-        #     [1,1,1,1],[1,1,-1,1],[1,-1,1,1],[1,-1,-1,1]
-        # ])
-        # self.faces = np.array([
-        #     [0,1,3,2],[3,2,4,5],[4,5,7,6],[7,6,0,1],[0,2,4,6],[7,1,3,5]
-        # ])
+        self.texture = texture
+        self.vertices, self.faces, self.uv, self.tex_map, self.textured = self.open_obj(self.file_name, texture)
 
-    def open_obj(self, filename):
+    def open_obj(self, filename, texture):
         vertices = []
         faces = []
+        uv = []
+        textures_map = []
         with open(filename) as file:
             for line in file:
                 if line.startswith('v '):
-                    lines = line[2:].split() + [1]
+                    lines = line[2:].split()
                     v = [float(i) for i in lines]
                     vertices.append(v)
+                    
+                elif line.startswith('vt '):
+                    uv.append(line[3:].split())
+                    
                 elif line.startswith('f '):
                     lines_2 = line[2:].split()
                     fc = [i.split('/') for i in lines_2]
-                    vf = []
-                    for sub_list in fc:
-                        vf.append(int(sub_list[0])-1)
-                    faces.append(vf)
-        return np.array(vertices), faces
-    
+                    if len(fc) == 1:
+                        faces.append([lines_2[0], lines_2[1], lines_2[2]])
+
+                        if len(lines_2) > 3:
+                            faces.append([lines_2[0], lines_2[2], lines_2[3]])
+
+                    else:
+                        faces.append([fc[0][0],fc[1][0],fc[2][0]])
+                        textures_map.append([fc[0][1],fc[1][1],fc[2][1]])
+                        
+                        if len(fc) > 3:
+                            faces.append([fc[0][0], fc[2][0], fc[3][0]])
+                            textures_map.append([fc[0][1], fc[2][1], fc[3][1]])
+
+        vertices = np.asarray(vertices).astype(float)
+        faces = np.asarray(faces).astype(int)
+
+        if len(uv) > 0 and len(textures_map) > 0:
+            textured = True
+            uv = np.asarray(uv).astype(float)
+            uv[:, 1] = 1 - texture_uv[:, 1]
+            textures_map = np.asarray(textures_map).astype(int) - 1
+
+        else:
+            uv, textures_map = np.asarray(uv), np.asarray(texture_map)
+            textured = False
+
+        return vertices, faces, uv, textures_map, textured
+
     def rotation_x(self, angle):
         x = np.array([
             [1, 0, 0, 0],
@@ -82,20 +103,24 @@ class Object:
 
     def camera_relation(self):
         new_vertices = self.vertices - self.eng.cam.pos
+        new_vertices1 = new_vertices.copy()
+        
+        new_vertices1 = new_vertices @ self.eng.cam.yaw(-self.eng.cam.rot_cam[3] + math.pi / 2)
+        new_vertices = new_vertices1 @ self.eng.cam.pitch(-self.eng.cam.rot_cam[3])
+
+        new_vertices[:, 2][(new_vertices[:,2] < 0.01) & (new_vertices[:, 2] > -0.01)] = -0.01
+
         return new_vertices
 
     def draw(self):
-        # self.vertices = np.dot(self.vertices, self.rotation_x(math.radians(1)))
         vertices = self.camera_relation()
-        # print(f'1: {vertices[0]} \n')
         vertices = np.dot(vertices, self.eng.cam.project_matrix())
-        # print(f'2: {vertices[0]} \n')
         vertices /= vertices[:, -1].reshape(-1, 1)
-        # print(f'3: {vertices[0]} \n')
         vertices = np.dot(vertices, self.eng.cam.screen_projection())
-        # print(f'4: {vertices[0]} \n')
+
         for i in vertices[:,:2]:
             pygame.draw.circle(self.eng.screen, pygame.Color('red'), i, 2)
         vix = vertices[:,:2]
         for face in self.faces:
             pygame.draw.polygon(self.eng.screen, pygame.Color('white'), vix[face], 1)
+
